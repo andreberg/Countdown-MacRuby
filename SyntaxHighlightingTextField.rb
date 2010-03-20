51fa37f30de740b36a95f94b6e7fb43ad6509bd7
André Berg <andre.bergmedia@googlemail.com>
2010-03-20 07:15:33 +0100

    Add SyntaxHighlightingTextField class.
    Currently empty but may be used later to add syntax coloring to the action text textview in the preference panel.

---
+++ SyntaxHighlightingTextField.rb
@@ -0,0 +1,10 @@
+# SyntaxHighlightingTextView.rb
+# Countdown
+#
+# Created by Andre Berg on 06.03.10.
+# Copyright 2010 Berg Media. All rights reserved.
+
+class SyntaxHighlightingTextField < NSTextField
+
+
+end
