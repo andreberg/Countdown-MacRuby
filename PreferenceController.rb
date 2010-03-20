8f4184444327c092514b4054763e39d5c0efb823
André Berg <andre.bergmedia@googlemail.com>
2010-03-20 07:13:45 +0100

    PreferenceController changes. See below for specifics.
    1. add user defaults handling code.
    2. setupDefaults is now an instance method and called from init.
    3. new action method: logOutputVerboseClicked. Sends the notification to the app controller.

--- PreferenceController.rb
+++ PreferenceController.rb
@@ -4,6 +4,7 @@
 # Created by Andre Berg on 04.03.10.
 # Copyright 2010 Berg Media. All rights reserved.
 
+LogOutputVerboseChangedNotification = 'LogOutputVerboseChangedNotification'
 TimeUnitChangedNotification = 'TimeUnitChangedNotification'
 ActionTypeChangedNotification = 'ActionTypeChangedNotification'
 ActionTextChangedNotification = 'ActionTextChangedNotification'
@@ -11,50 +12,72 @@ ActionTextChangedNotification = 'ActionTextChangedNotification'
 ActionTypeKey = 'ActionType'
 ActionTextKey = 'ActionText'
 TimeUnitKey = 'TimeUnit'
-LastTimeValueKey = 'LastTimeValue'
+TimeValueKey = 'TimeValue'
+LogOutputVerboseKey = 'LogOutputVerbose'
+UpdateIntervalKey = 'UpdateInterval'
 
 
 class PreferenceController < NSWindowController
-
-   attr_accessor :actionType, :actionText, :timeUnit, :lastTimeValue
-
-#    Defaults = {
-#       ActionTypeKey => 'Dialog + Beep',
-#       ActionTextKey => 'Countdown has ended...',
-#       TimeUnitKey => 'seconds',
-#       LastTimeValueKey => '1.0',
-#       LogOutputVerbose => true
-#    }
    
-   def self.setupDefaults
-      defaultsPlist = NSBundle.mainBundle.pathForResource "Defaults", ofType:"plist"
-      defaultsDict = NSDictionary.dictionaryWithContentsOfFile defaultsPlist
-      p defaultsDict
-      NSUserDefaults.standardUserDefaults.registerDefaults defaultsDict
-      NSUserDefaultsController.sharedUserDefaultsController.setInitialValues defaultsDict
-   end
-
+   attr_accessor :appController
+   attr_reader :logOutputVerbose
+   
    def init
       initWithWindowNibName('Preferences')
+      
+      self.setupDefaults
+
       defaults = NSUserDefaults.standardUserDefaults
       
       @actionType = defaults.stringForKey(ActionTypeKey)
       @actionText = defaults.stringForKey(ActionTextKey)
       @timeUnit = defaults.stringForKey(TimeUnitKey)
-      @timeValue = defaults.stringForKey(LastTimeValueKey)
-      
+      @timeValue = defaults.doubleForKey(TimeValueKey)
+      @logOutputVerbose = defaults.boolForKey(LogOutputVerboseKey)
+      @updateInterval = defaults.stringForKey(UpdateIntervalKey)
+
       self
+   end
+
+   def logOutputVerboseClicked(sender)
+      if sender.state == 0
+         @logOutputVerbose = false
+      else
+         @logOutputVerbose = true
+      end
+      info = { "newValue" => @logOutputVerbose }
+      $center.postNotificationName LogOutputVerboseChangedNotification, object:$prefs, userInfo:info
    end
 
    def self.sharedPreferenceController
       @instance ||= alloc.init
    end
    
-#    def showWindow(sender)
-#       self.window
-#    end
-
-   def windowDidLoad
-      
+   def setupDefaults
+      defaultsPlist = NSBundle.mainBundle.pathForResource "Defaults", ofType:"plist"
+      defaultsDict = NSDictionary.dictionaryWithContentsOfFile defaultsPlist
+      $defaults.registerDefaults defaultsDict
+      NSUserDefaultsController.sharedUserDefaultsController.setInitialValues defaultsDict
+   end
+   
+   def showWindow(sender)
+      if window == nil
+         NSBundle.loadNibNamed("Preferences", owner:self)
+      end
+      NSApp.beginSheet(window, modalForWindow:NSApp.mainWindow, modalDelegate:self, didEndSelector:'didEndSheet:', contextInfo:nil)
+   end
+   
+   def savePrefsAndCloseWindow(sender)
+      NSUserDefaultsController.sharedUserDefaultsController.save(nil)
+      NSUserDefaults.standardUserDefaults.synchronize()
+      NSApp.endSheet window
+   end
+   
+   def closeWindow(sender)
+      NSApp.endSheet window
+   end
+   
+   def didEndSheet(sheet)
+      sheet.orderOut self
    end
 end
